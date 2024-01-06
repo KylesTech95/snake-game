55cb26f83bdc958f485943694ec783ee7cc492c9
kylestech95 <kylestech95@outlook.com>
2024-01-06 18:59:32 -0500

    test: move-snake

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import { useEffect, useRef,useState } from 'react';
 import './App.css';
 
-function StartBtn(){
+function StartBtn({moveSnake}){
   return (
     <div className="startBtn-container">
-      <button id="startBtn">Start</button>
+      <button id="startBtn" onClick={moveSnake}>Start</button>
     </div>
   )
 }
-function Snake({snakeRef}){
+function Snake({snakeRef,snake}){
   return (
-    <div id="snake" ref={snakeRef}></div>
+    <div id="snake" ref={snakeRef} style={{'left':`${snake[0].x}px`,'top':`${snake[0].y}px`}}></div>
   )
 }
 function App() {
@@ -19,14 +19,23 @@ function App() {
   let canvasRef=useRef()
   let snakeRef=useRef()
   let unitSize=25
+  const [snake,setSnake]=useState([{
+    x:0,
+    y:0
+  }])
 
+  const moveSnake = () => {
+    setInterval(()=>{
+      setSnake(s=>[{x:s[0].x+unitSize,y:s[0].y}])
+      },1000)
+  }
   return (
     <div id="canvas-container" ref={canvasRef}>
       <canvas id="canvas-actual" height="500" width="500"/>
-      <Snake {...{snakeRef}}/>
+      <Snake {...{snakeRef, snake}}/>
 
       {/*Start button*/}
-      <StartBtn/>
+      <StartBtn {...{moveSnake}}/>
     </div>
   );
 }
