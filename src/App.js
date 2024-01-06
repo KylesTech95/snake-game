298f53264cef20d4068e3556bf1ddecca8b68091
kylestech95 <kylestech95@outlook.com>
2024-01-06 07:11:53 -0500

    feat: add start btn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import { useEffect, useRef,useState } from 'react';
 import './App.css';
 
+function StartBtn(){
+  return (
+    <div className="startBtn-container">
+      <button id="startBtn">Start</button>
+    </div>
+  )
+}
 function Snake({snakeRef}){
   return (
     <div id="snake" ref={snakeRef}></div>
@@ -11,26 +18,28 @@ function App() {
   //global  
   let canvasRef=useRef()
   let snakeRef=useRef()
-  let unitSize=5;
   const [sides,setSides] = useState({x:undefined,y:undefined})
-useEffect(()=>{
-  let tmp_sides = {
-    x: canvasRef.current.getBoundingClientRect().x,
-    y: canvasRef.current.getBoundingClientRect().y
-  }
-  let bod = document.querySelector('body')
-  let bodWidth = bod.getBoundingClientRect().x;
-  let bodHeight = bod.getBoundingClientRect().y;
-  setSides(sides.x=(bodWidth/tmp_sides.x),
-           sides.y=(bodHeight/tmp_sides.y)  )
-  // declare snake's position
-  snakeRef.current.style=`left:${sides.x}px;top:${sides.y}px;`
-},[])
+  useEffect(()=>{
+    let tmp_sides = {
+      x: canvasRef.current.getBoundingClientRect().x,
+      y: canvasRef.current.getBoundingClientRect().y
+    }
+    let bod = document.querySelector('body')
+    let bodWidth = bod.getBoundingClientRect().x;
+    let bodHeight = bod.getBoundingClientRect().y;
+    setSides(sides.x=(bodWidth/tmp_sides.x),
+            sides.y=(bodHeight/tmp_sides.y)  )
+    // declare snake's position
+    snakeRef.current.style=`left:${sides.x}px;top:${sides.y}px;`
+  },[])
 
   return (
     <div id="canvas-container" ref={canvasRef}>
       <canvas id="canvas-actual" height="500" width="500"/>
       <Snake {...{snakeRef}}/>
+
+      {/*Start button*/}
+      <StartBtn />
     </div>
   );
 }
