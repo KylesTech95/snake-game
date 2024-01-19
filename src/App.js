ae859952e7fec4f715ec9785d78808651cd79f93
kylestech95 <kylestech95@outlook.com>
2024-01-19 09:45:25 -0500

    fork: bye snake

--- src/App.js
+++ src/App.js
@@ -1,59 +1,6 @@
 import { useEffect, useRef,useState,useCallback } from 'react';
 import './App.css';
 
-
-function Snake({snakeRef,snake,playing,gameover,score,setSnake,unitSize}){
-  const min = 0;
-  const max = 475;
-  const [snake2,setSnake2] = useState([{x:0,y:0},{x:unitSize,y:0},{x:unitSize*2,y:0}])
-  useEffect(()=>{
-    if(!playing){
-      window.addEventListener('keydown',e=>{
-      return null 
-      })
-    }
-  },[snakeRef,playing])
-
-  const handleKey = (event) => {
-    if(event.key==='d'){
-      console.log(event.key)
-      for(let body of snake2){
-        console.log(body)
-        body.x+=unitSize
-      }
-    }
-  }
-  const memoizedListener = useCallback(handleKey, [])
-useEffect(()=>{
-  if(gameover && score > 0){
-    console.log('game is over ')
-  }
-    //update snake direction
-  if(playing){
-    window.addEventListener('keypress',memoizedListener)
-    return () => {
-      window.removeEventListener('keypress',memoizedListener);
-    }
-  }
-  else{
-    return () => {
-      window.removeEventListener('keypress',memoizedListener);
-    };
-  }
-
- },[playing,memoizedListener])
-
-  return (
-    <>
-    {snake2.map((snek,index)=>(
-      <div className="snake" ref={snakeRef} key={index} style={{'left':`${snek.x}px`,'top':`${snek.y}px`}}></div>
-    ))}
-    </>
-    
-  )
-  
-}
-
  function Food({food,setFood}) {
 
   return (
@@ -62,7 +9,6 @@ useEffect(()=>{
     </div>
   )
 }
-
 function ScoreBoard({score,setScore}){
   return(
   <div className="score-container">
@@ -119,19 +65,11 @@ function Btn({startGame,playing,setPlaying,gameover,setGameover,resetGame,btnCol
 
 
 
-
-
-
-
-
-
 function App() {
   //global
   let canvasRef=useRef()
-  let snakeRef=useRef()
   let btnRef = useRef();
   let unitSize = 25;
-  const [snake,setSnake]=useState([{x:0,y:0},{x:unitSize,y:0},{x:unitSize*2,y:0}])
   const [food,setFood]=useState([{x:undefined,y:undefined}])
   const [playing,setPlaying]=useState(false)
   const [gameover,setGameover]=useState(true)
@@ -164,7 +102,6 @@ function App() {
       //set playing to true
       setPlaying(false)
       setGameover(true)
-      setSnake([{x:0,y:0}])
       setBtnColor('green')
       btnRef.current.addEventListener('mouseover',e=>{
         let btn = e.target;
@@ -183,7 +120,6 @@ function App() {
   return (
     <div id="canvas-container" ref={canvasRef}>
       <canvas id="canvas-actual" height="500" width="500"/>
-      <Snake {...{gameover,score,resetGame,snakeRef,snake,playing,unitSize}}/>
       <Food {...{food,setFood}}/>
       <ScoreBoard  {...{score}}/>
       {/*Start button*/}
