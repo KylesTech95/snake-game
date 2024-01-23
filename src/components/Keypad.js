1ff412054a3f939a04805ec9e638f3ff82b44ae4
kylestech95 <kylestech95@outlook.com>
2024-01-23 11:04:34 -0500

    fix/keypad onload

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,16 +5,16 @@ export default function Keypad({setKeypadHidden,keypadHidden,keypadRef}) {
         let keys = document.querySelectorAll('.k')
         for(let x = 0; x < keys.length; x++){
             if(x===0){
-                keys[x].style=`left: 25px;`
+                keys[x].style=`left: 25px;position:absolute;`
             }
             if(x===1){
-                keys[x].style=`right: 25px;`
+                keys[x].style=`right: 25px;position:absolute;`
             }
             if(x===2){
-                keys[x].style=`left:${keypadRef.current.getBoundingClientRect().width/2.5}px;top: 0px`
+                keys[x].style=`left:${keypadRef.current.getBoundingClientRect().width/2.5}px;top: 0px;position:absolute;`
             }
             if(x===3){
-                keys[x].style=`left:${keypadRef.current.getBoundingClientRect().width/2.5}px;bottom: 0px`
+                keys[x].style=`left:${keypadRef.current.getBoundingClientRect().width/2.5}px;bottom: 0px;position:absolute;`
             }
         }
         if(window.innerWidth <= 1300){
